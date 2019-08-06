65cfd435a05b9aca6aa9a93dd4fa183389044d94
tomoaki-kimura <tkim19750404@gmail.com>
2019-08-06 11:03:45 +0900

    add_list

--- app.rb
+++ app.rb
@@ -8,18 +8,25 @@ get '/' do
   @fizz_buzz = []
   @fizz = []
   @buzz = []
+  @results = []
   (start..goal).each do |n|
     case
     when n % 3 == 0 && n % 5 == 0
       @fizz_buzz << n
+      @results << 'FizzBuzz'
     when n % 3 == 0
       @fizz << n
+      @results << 'Fizz'
     when n % 5 == 0
       @buzz << n
+      @results << 'Buzz'
+    else
+      @results << n
     end
   end
   # ここまでコードを入力して下さい。
   @fizz_buzz = [] if @fizz_buzz.include?(0)
+  @results = [] if @results == ["FizzBuzz"]
   erb :index
 end
 
