f8d3c8ef2d28309e1748ac6ec3fb285baf17575f
tomoaki-kimura <tkim19750404@gmail.com>
2020-01-07 07:39:39 +0900

    for answer

--- app.rb
+++ app.rb
@@ -1,33 +1,25 @@
 require 'sinatra'
 
 get '/' do
-  # ここにコードを入力してください。
-  start = params[:start].to_i
-  goal = params[:goal].to_i
-
-  @fizz_buzz = []
-  @fizz = []
-  @buzz = []
+  number_start = params[:number_start].to_i
+  number_end = params[:number_end].to_i
   @results = []
-  (start..goal).each do |n|
+
+  # ここにコードを入力してください。
+  (number_start..number_end).each do |n|
     case
     when n % 3 == 0 && n % 5 == 0
-      @fizz_buzz << n
-      @results << 'FizzBuzz'
+      @results << [n, 'FizzBuzz']
     when n % 3 == 0
-      @fizz << n
-      @results << 'Fizz'
+      @results << [n, 'Fizz']
     when n % 5 == 0
-      @buzz << n
-      @results << 'Buzz'
+      @results << [n, 'Buzz']
     else
-      @results << n
+      @results << [n, n]
     end
   end
   # ここまでコードを入力して下さい。
-  @fizz_buzz = [] if @fizz_buzz.include?(0)
-  @results = [] if @results == ["FizzBuzz"]
+
+  @results = [] if @results == [[0, "FizzBuzz"]]
   erb :index
 end
-
-# Lesson3変数を使ってプログラミング
